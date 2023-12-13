a74c061773ae571ab86c802b550ee788a91cf744
minimirror1 <minimirror1@gmail.com>
2023-12-13 20:54:21 +0900

    상태값 추가

--- Motor.h
+++ Motor.h
@@ -13,9 +13,14 @@
 class Motor {
 public:
     enum Status {
-        Status_None = 0,
-        Status_Run,
-        Status_Error
+        Status_None = 0,	//초기상태
+		Status_Init,		//초기화 진행
+		Status_PreRun,		//초기화 완료, 위치 동기화x
+		Status_PosiSync,	//위치 동기화
+        Status_Run,			//위치 동기화 완료
+
+		Status_Error,		//동작중 에러
+		Status_InitError	//초기화 에러
     };
 
     enum MotorType {
@@ -32,13 +37,17 @@ public:
         uint16_t reducer_ratio;
     };
 
+    struct Monitor {
+    	int32_t raw_current_posi;
+    };
+
     virtual ~Motor() {}
 
     /* input 필수 기능 */
-    // init
+    /* init */
     virtual void setSettingInfo(uint8_t dir, uint16_t angle, uint16_t initPosi, uint16_t reducer_ratio) = 0;
 
-    // control
+    /* control */
     virtual void setPosition(uint16_t targetPosition) = 0;
 
     /* input 옵션 기능 */
@@ -46,6 +55,10 @@ public:
     /* output 필수 기능*/
     virtual uint16_t getPosition() const = 0;
     /* output 옵션 기능 */
+
+    /* 공통 funtion */
+    virtual void init() = 0;
+    virtual void defaultPosi() = 0;
 
 protected:
     bool id_check(uint8_t gID, uint8_t sID) {
@@ -64,6 +77,7 @@ protected:
 
     /* Motor Setting */
     MotorSetting setting_;
+    Monitor monitor_;
 
     // 부모 클래스 생성자에 의해 호출되는 초기화 함수
     Motor(uint8_t gID, uint8_t sID, MotorType motorType)
